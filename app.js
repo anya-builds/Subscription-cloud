3e657f8d69e92db20577f30ee4ec3a6045c11fe3
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-10-27 22:25:35 +0530

    subplan

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("server running")
+
+import express from 'express'
+
+const app=express();
+
+app.get('/',(req, res)=>{
+    res.send('Welcome to the subscription tracker API!');
+})
+
+app.listen(3000, ()=>{
+    console.log('Subscription Tracker API is running on http://localhost:3000');
+})
+
+export default app;
