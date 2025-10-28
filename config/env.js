b9d653ced9c8499afeee2237467addd5bf6d4f0a
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-10-28 15:30:54 +0530

    subplan

--- config/env.js
+++ config/env.js
@@ -1,7 +1,12 @@
 import { config } from "dotenv";
-// import PORT, NODE_ENV from ""
+
 
 
 config({path:`.env.${process.env.NODE_ENV || 'development'}.local`});
 
-export const {PORT, NODE_ENV, DB_URI} = process.env;
+export const {PORT,
+     NODE_ENV, 
+     DB_URI,
+    JWT_SECRET,
+    JWT_EXPIRES_IN
+    } = process.env;
