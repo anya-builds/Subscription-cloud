02c011a968e1ed747d34580aa926fa651c28dd5d
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-10-28 12:03:14 +0530

    subplan

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ import userRouter from './routes/user.routes.js'
 
 const app=express();
 
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/users', userRouter)
+app.use('/api.v1/subscriptions',subscriptionRouter)
+
 app.get('/',(req, res)=>{
     res.send('Welcome to the subscription tracker API!');
 })
--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -4,4 +4,18 @@ const subscriptionRouter = Router();
 
 subscriptionRouter.get('/',(req,res)=> res.send({title: 'GET all subscription'}))
 
+subscriptionRouter.get('/:id',(req,res)=> res.send({title:'GET subscription details'}))
+
+subscriptionRouter.post('/', (req,res)=> res.send({title:'CREATE subscription'}))
+
+subscriptionRouter.put('/:id', (req,res)=> res.send({title:'UPDATE subscription'}))
+
+subscriptionRouter.delete('/:id',(req,res)=> res.send({title:'DELETE subscription'}))
+
+subscriptionRouter.get('/user/:id',(req,res)=> res.send({title: 'GET all user subscriptions'}))
+
+subscriptionRouter.put('/:id/cancel',(req,res)=> res.send({title: 'CANCEL susbcription'}))
+
+subscriptionRouter.get('/upcoming-renewals',(req,res)=> res.send({title:'GET upcming renewals'}))
+
 export default subscriptionRouter;
