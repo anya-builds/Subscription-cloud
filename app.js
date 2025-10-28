f70a7b4865149c9288a99ab2611ef0428e919f73
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-10-28 12:19:06 +0530

    subplan

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app=express();
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
-app.use('/api.v1/subscriptions',subscriptionRouter)
+app.use('/api/v1/subscriptions',subscriptionRouter)
 
 app.get('/',(req, res)=>{
     res.send('Welcome to the subscription tracker API!');
