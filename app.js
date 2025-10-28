90b9097b3da17001aef4115231ed6c6d82940b00
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-10-28 11:40:54 +0530

    subplan

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 
 import express from 'express'
 import { PORT } from './config/env.js';
+import subscriptionRouter from './routes/subscription.routes.js';
+import authRouter from './routes/auth.routes.js'
+import userRouter from './routes/user.routes.js'
 
 const app=express();
 
--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,3 +1,7 @@
 import { Router } from "express";
 
 const subscriptionRouter = Router();
+
+subscriptionRouter.get('/',(req,res)=> res.send({title: 'GET all subscription'}))
+
+export default subscriptionRouter;
